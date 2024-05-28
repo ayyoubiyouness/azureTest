5bcf260dff8addb8be61c6af1bcf3c695345cce3
Ayyoubi <114993701+ayyoubiyouness@users.noreply.github.com>
2024-05-28 21:40:39 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('This message is from Youness : My heart Rania All I need is you right here. I looooooooooove you so muuch ');
+  res.send('hello world ');
 });
 
 app.listen(port, () => {
